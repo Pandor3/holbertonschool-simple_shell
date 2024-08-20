fcf67006e7041bfe1e3030c755d639196b563079
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-20 08:41:55 +0000

    updating main file

--- main.c
+++ main.c
@@ -4,36 +4,22 @@
  * main - infinity loop at the core of the simple shell
  * Return: 0 on success, exit with 0 on fail
 */
-int main(void)
+int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
 
-    while (1)
-    {
-        printf("$ ");  // Afficher le prompt
+	while (1)
+	{
+		printf("$ ");  /* Afficher le prompt */
 
-        nread = getline(&line, &len, stdin);  // Lire la commande entrée par l'utilisateur
-        if (nread == -1)
-        {
-            if (feof(stdin))
-            {
-                free(line);
-                exit(0);  // Sortie propre si l'utilisateur utilise Ctrl+D
-            }
-            perror("getline");
-            free(line);
-            exit(1);
-        }
-
-        // Pour l'instant, on affiche juste la commande pour vérification
-        printf("Vous avez entré : %s", line);
-        
-        // On ajoutera ici le code pour traiter et exécuter la commande
-
-    }
-
-    free(line);
-    return 0;
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			perror("getline");
+			free(line);
+			exit(1);
+		}
+	}
+	free(line);
+	return (0);
 }
