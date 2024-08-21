9e5eef396cbf8e7a620d8d007896b21e637929d0
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-21 07:58:04 +0000

    adding files for test

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+char *input(void);
+char **commands(char *buffer, const char *str);
+
+
+#endif
