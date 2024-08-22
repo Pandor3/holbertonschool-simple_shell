9d73beb81d9890d9f6365f9f57153d05e26d99c8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-22 13:46:26 +0000

    updating main file

--- main.c
+++ main.c
@@ -1,39 +1,75 @@
 #include "main.h"
 
-/**
- * main - infinite loop at the core of the simple shell
- * @argc: arguments count
- * @argv: list of arguments
- * @envp: environnement variable
- *
- * Return: 0 on success, exit with 0 on fail
-*/
-int main(int argc, char **argv, char **envp)
+int main(void)
 {
 	char *line;
 	char **args;
-
-	(void)argv;
-	(void)argc;
+	char *cmd;
+	int status;
 
 	while (1)
 	{
-		printf("$");
+		printf("$ ");
 		line = input();
-		if (line == NULL)
-			break;
 
-		if (strlen(line) == NULL)
+		/* Tokenization de la ligne de commande */
+		args = commands(line, " \t\r\n\a");
+
+		if (args == NULL)
 		{
 			free(line);
 			continue;
 		}
-		if (args[0] != NULL)
-		commands(args, envp, line);
 
-		free_continue(args);
-		free(line);
+		cmd = args[0];
+
+		/* Gérer les commandes intégrées */
+		if (strcmp(cmd, "cd") == 0)
+		{
+			if (args[1] == NULL)
+			{
+				fprintf(stderr, "expected argument to \"cd\"\n");
+			}
+			else
+			{
+				changedir(args[1]);
+			}
+		}
+		else if (strcmp(cmd, "exit") == 0)
+		{
+			free_memory(line, args);
+			exit(0);
+		}
+		else if (strcmp(cmd, "printenv") == 0)
+		{
+			printenv(environ);
+		}
+		else
+		{
+			/* Exécution des autres commandes */
+			pid_t pid = fork();
+			if (pid == 0)
+			{
+				/* Processus enfant */
+				if (execvp(args[0], args) == -1)
+				{
+					perror("Error");
+				}
+				exit(EXIT_FAILURE);
+			}
+			else if (pid < 0)
+			{
+				perror("Error forking");
+			}
+			else
+			{
+				/* Processus parent */
+				wait(&status);
+			}
+		}
+
+		free_memory(line, args);
 	}
-	free(line);
+
 	return (0);
 }
